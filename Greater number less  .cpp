60d6600c22429c5c36b21499b3fa7c7c9fb49e54
gayatrithombare <gayatri.thombare24@sanjivani.edu.in>
2024-11-20 20:27:34 +0530

    Greater number less  .cpp

---
+++ Greater number less  .cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int number;
+
+Cout <<" Enter a number";
+
+CPN >> number;
+
+If (humber>10) {
+
+Cout <<" The number is greater than 10." << endl;
+
+} else if (number <10) {
+
+cout <<" The number is less than 10" << endl;
+
+} else
+
+{
+
+Cout << " The number is ten 人人 endl;
+}
+return 0;
+}
