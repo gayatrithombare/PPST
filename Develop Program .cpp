fe4ef19f54890809172e5f029cbef76c7075a549
gayatrithombare <gayatri.thombare24@sanjivani.edu.in>
2024-11-20 19:38:02 +0530

    Develop Program .cpp

---
+++ Develop Program .cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+int main ()
+
+double mike Distance, sarah Distance,
+
+cout <<" Enter the distance covered by mike (inkm)",
+
+cin mike Distance,
+
+cout<<" Enter the distance covered by sarah (In km):";
+
+Cin >> sarah Distance
+
+If (mike Distance > sarah Distance) {
+
+cout <<"raike is ahead by" <<
+
+mike Distance <<<" kilometers." << endl;
+
+}
+
+else if (Sorah Distance > mike Distance) {
+
+Cout << Sarrah is ahead by" <<
+
+Sarah Distance <<" kilometers.""<<endl;
+
+else{
+
+Cout <<" mike and Sarah are at the Same point in the race "<<endl;
+}
+return 0;
+
+}
