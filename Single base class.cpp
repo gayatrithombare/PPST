6e1db32b7cbc8c5c625150ec7eaed97b03246759
gayatrithombare <gayatri.thombare24@sanjivani.edu.in>
2024-11-22 23:06:15 +0530

    Single base class.cpp

---
+++ Single base class.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+closs A 
+{ 
+Public: 
+int x,y; 
+Vord getdata()
+{ 
+cout<<"Enter volue of x & y!";
+cin>>>>y;
+}
+}; 
+class B: public A 
+{
+public:
+vold product() 
+} 
+cout << "Product = " <<x*y; 
+} 
+};
+Class C; public A
+public:
+void sum() 
+{ 
+cout << "Sum = "<<X+Y;
+}
+};
+
+int main ()
+{ 
+Boby 1;
+C obj 2;
+obj 1. getdata();
+obj 1. product();
+obj2 getdata();
+Obj2. sum(),
+return 0; 
+} 
+
+
+output: Enter value of x and y:6 7 Product =42 Enter value of x and y: 9 Sum=18
